66b56f532178f6f89ea5748c853202b7bc218c59
Peter Backx <peter.backx@gmail.com>
2021-05-16 15:59:35 +0200

    Server will store all files for further analysis and retraining

--- analysis/predict_server.py
+++ analysis/predict_server.py
@@ -1,13 +1,25 @@
+import os
+from datetime import datetime
+
 from flask import Flask, request
 import predict_brick
 
 app = Flask(__name__)
-
+base_dir = 'C:\\dev\\lego-sorter-pi\\data\\new'
+temp_file = 'C:\\dev\\lego-sorter-pi\\data\\tmp\\img.jpg'
 
 @app.route("/predict", methods=['POST'])
 def predict():
     image = request.files['image']
-    image.save('C:\\dev\\lego-sorter-pi\\data\\tmp\\img.jpg')
+    image.save(temp_file)
+    label = predict_brick.predict(temp_file)
+
+    labeled_dir = f'{base_dir}\\{label}'
+    os.makedirs(labeled_dir, exist_ok=True)
+
+    filename = datetime.now().strftime("%Y%m%d%H%M%S") + ".jpg"
+    image.save(f'{labeled_dir}\\{filename}')
+
     return {
-        "prediction": predict_brick.predict('C:\\dev\\lego-sorter-pi\\data\\tmp\\img.jpg')
+        "prediction": label
     }
