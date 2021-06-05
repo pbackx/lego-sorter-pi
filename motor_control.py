5b7a2a3c97017b6d621c73c8f6aa95313809c84e
Peter Backx <peter.backx@gmail.com>
2021-06-05 09:39:21 +0200

    added buckets to collect the bricks

--- motor_control.py
+++ motor_control.py
@@ -9,6 +9,9 @@ class MotorControl:
         self.camera_belt = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_A)
         self.main_belt = self.ev3dev2_motor.LargeMotor(self.ev3dev2_motor.OUTPUT_B)
         self.hopper_belt = self.ev3dev2_motor.LargeMotor(self.ev3dev2_motor.OUTPUT_C)
+        self.turntable = self.ev3dev2_motor.MediumMotor(self.ev3dev2_motor.OUTPUT_D)
+        self.turntable_rotations_per_bucket = 3.3
+        self.turntable_current_position = 0
         
     def on_all(self):
         self.hopper_belt.on(self.ev3dev2_motor.SpeedPercent(60))
@@ -26,4 +29,18 @@ class MotorControl:
         
     def clear_camera_belt(self):
         self.camera_belt.on_for_rotations(self.ev3dev2_motor.SpeedPercent(50), 10)
+    
+    def tune_turntable(self, amount = -0.1):
+        turntable.on_for_rotations(motor_control.ev3dev2_motor.SpeedPercent(50), amount)
         
+    def move_turntable(self, position):
+        # convert to degrees
+        current_position_degrees = self.turntable_current_position * 60
+        position_degrees = position * 60
+        # angle between them
+        diff_degrees = position_degrees - current_position_degrees
+        diff_degrees = ((diff_degrees + 180) % 360) - 180
+        # convert back
+        positions_to_move = diff_degrees / 60
+        self.turntable_current_position = position
+        self.turntable.on_for_rotations(self.ev3dev2_motor.SpeedPercent(50), positions_to_move * 3.3)
